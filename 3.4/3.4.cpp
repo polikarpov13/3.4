37f31e19e28890c4708918a05b48b9ad20fd0e63
polikarpov13 <vladyslav.polikarpov.ik.2021@lpnu.ua>
2021-09-28 17:57:46 +0300

    master commit

---
+++ 3.4/3.4.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double x;
+	double y;
+	double R;
+
+
+	cout << "X = ";
+	cin >> x;
+
+	cout << endl;
+
+	cout << "Y = ";
+	cin >> y;
+
+	cout << endl;
+
+	cout << "R = ";
+	cin >> R;
+
+	cout << endl;
+
+
+	if ((y <= sqrt(pow(R, 2) - pow(x, 2))) && y >= 0 && x >= 0 ||
+		(y <= 0) && (y >= (-R / R) * (x + R)) && x <= 0)
+		cout << "Yes";
+	else
+		cout << "No";
+
+
+	system("pause");
+	return 0;
+}
